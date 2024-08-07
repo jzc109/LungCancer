6fd1c1624f10b874f11329885adbb0053519dfa2
Zhichao Jin <jzc109@users.noreply.github.com>
2024-08-07 16:13:53 +0800

    Update 1_CT Image Illustration.py

--- pages/1_CT Image Illustration.py
+++ pages/1_CT Image Illustration.py
@@ -1,10 +1,27 @@
-import streamlit as st
-from PIL import Image
+import streamlit as st  
+import requests  
+from PIL import Image  
+from io import BytesIO  
 
-imageCT = Image.open('https://raw.githubusercontent.com/jzc109/LungCancer/main/pages/CT_image.png"')
-st.set_page_config(page_title="CT image Demo")
+# GitHub图片的URL  
+url = 'https://raw.githubusercontent.com/jzc109/LungCancer/main/pages/CT_image.png'  
 
-st.markdown("# CT image illustration")
-st.write(' ')
-st.image(imageCT)
+# 获取图片  
+response = requests.get(url)  
+
+# 检查请求是否成功  
+if response.status_code == 200:  
+    # 使用BytesIO将响应内容转换为图像  
+    imageCT = Image.open(BytesIO(response.content))  
+else:  
+    st.error("无法加载图片。请检查URL或网络连接。")  
+
+# 设置页面配置  
+st.set_page_config(page_title="CT Image Demo")  
+
+# 显示标题和图片  
+st.markdown("# CT Image Illustration")  
+st.write(' ')  
+if 'imageCT' in locals():  
+    st.image(imageCT)  
 st.write(' ')
