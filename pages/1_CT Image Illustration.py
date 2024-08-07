83881af096320bc1dab7e73e39463f94dc55311d
Zhichao Jin <jzc109@users.noreply.github.com>
2024-08-07 16:12:08 +0800

    Update 1_CT Image Illustration.py

--- pages/1_CT Image Illustration.py
+++ pages/1_CT Image Illustration.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-imageCT = Image.open('CT_image.png')
+imageCT = Image.open('https://raw.githubusercontent.com/jzc109/LungCancer/main/pages/CT_image.png"')
 st.set_page_config(page_title="CT image Demo")
 
 st.markdown("# CT image illustration")
